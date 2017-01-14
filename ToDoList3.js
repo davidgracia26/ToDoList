20785dd550f9dee339e59d0a25a3d32592f05c52
davidgracia26 <david.gracia26@gmail.com>
2017-01-14 16:26:58 -0600

    Now displays session storage array

--- ToDoList3.js
+++ ToDoList3.js
@@ -1,115 +1,92 @@
-//Establishes object
+var myArray = [];
+
+window.onload = function() {
 
 
+    var unparsedToDoArray = sessionStorage.getItem("toDoArray");
 
-var toDoItem = {
-id: "0",
-listItem: "Get Started With a List Item",
-dateCreated: new Date,
-isCompleted: false,
+    var parsedToDoArray = JSON.parse(unparsedToDoArray);
+
+    if (parsedToDoArray != null) {
+
+
+        for (var i = 0; i < parsedToDoArray.length; i++) {
+
+            //Creates the list row on the page with entry
+            var row = document.createElement("tr")
+            var td1 = document.createElement("td")
+            var td2 = document.createElement("td")
+            var td3 = document.createElement("td")
+            var td4 = document.createElement("td")
+            var input = document.createElement("input")
+            input.setAttribute("type", "checkbox")
+
+            var tn1 = document.createTextNode(parsedToDoArray[i].id)
+            var tn2 = document.createTextNode(parsedToDoArray[i].listItem)
+            var tn3 = document.createTextNode(parsedToDoArray[i].dateCreated)
+            // var rowh = document.createTextNode(input)
+
+            row.appendChild(td1).appendChild(tn1)
+            row.appendChild(td2).appendChild(tn2)
+            row.appendChild(td3).appendChild(tn3)
+            row.appendChild(td4).appendChild(input)
+
+            //Adds list row to page
+            document.getElementById("movingTable").appendChild(row)
+        }
+    }
 }
 
-//Places object in the array
-var myArray = []
-myArray[0] = toDoItem
-console.log(myArray)
+//Function for Submit button at bottom of page
+function createRow() {
 
+    var unparsedToDoArray = sessionStorage.getItem("toDoArray");
 
+    var parsedToDoArray = JSON.parse(unparsedToDoArray);
 
+    if (parsedToDoArray == null) {
 
-  //Function for Submit button at bottom of page
-function toss() {
-  //Creates new array item with toDoItem in new index
-  var vist = String(myArray.length)
-  var visty = parseInt(vist)
+        parsedToDoArray = [];
+    }
+    myArray = parsedToDoArray;
 
-  myArray[visty] = {
-    id: "0",
-    listItem: "Get Started With a List Item",
-    dateCreated: new Date,
-    isCompleted: false
-  }
-  myArray[visty].id = vist
+    myArray[myArray.length] = {
+        id: myArray.length,
+        listItem: document.getElementById("newToDo").value,
+        dateCreated: new Date(),
+        isCompleted: false
+    }
 
+    //Creates the list row on the page with entry
+    var row = document.createElement("tr")
+    var td1 = document.createElement("td")
+    var td2 = document.createElement("td")
+    var td3 = document.createElement("td")
+    var td4 = document.createElement("td")
+    var input = document.createElement("input")
+    input.setAttribute("type", "checkbox")
 
-  //Creates date property
-  var d = new Date()
-  myArray[visty].dateCreated = d
+    var tn1 = document.createTextNode(myArray[myArray.length - 1].id)
+    var tn2 = document.createTextNode(myArray[myArray.length - 1].listItem)
+    var tn3 = document.createTextNode(myArray[myArray.length - 1].dateCreated)
+    // var rowh = document.createTextNode(input)
 
-  //Takes the entry and puts it in the listItem property
-  var yar = document.getElementById("newToDo").value
-  myArray[visty].listItem = yar
+    row.appendChild(td1).appendChild(tn1)
+    row.appendChild(td2).appendChild(tn2)
+    row.appendChild(td3).appendChild(tn3)
+    row.appendChild(td4).appendChild(input)
 
-  //Creates the list row on the page with entry
-  var ban = document.createElement("tr")
-  var bana = document.createElement("td")
-  var banb = document.createElement("td")
-  var banc = document.createElement("td")
-  var band = document.createElement("td")
-  var banj = document.createElement("input")
-  banj.setAttribute("type", "checkbox")
+    //Adds list row to page
+    document.getElementById("movingTable").appendChild(row)
 
-  var bane = document.createTextNode(vist)
-  var banf = document.createTextNode(yar)
-  var bang = document.createTextNode(d)
-  var banh = document.createTextNode(banj)
+    var stringifiedArray = JSON.stringify(myArray);
 
-  ban.appendChild(bana).appendChild(bane)
-  ban.appendChild(banb).appendChild(banf)
-  ban.appendChild(banc).appendChild(bang)
-  ban.appendChild(band).appendChild(banj)
-
-  //Adds list row to page
-  document.getElementById("movingTable").appendChild(ban)
-
-  console.log(myArray)
-
-  //Store session until tab closes
-
-  var change = JSON.stringify(myArray);
-
-  var myStorage = sessionStorage.setItem("idee",change)
-
-  var message = sessionStorage.getItem("idee")
-
-  var products = JSON.parse(message)
-
-  console.log(products)
-  //console.log(sessionStorage.getItem("idee"))
-
-
-  function ross() {
-
-    //if checkbox checked, removes that row
-    console.log(myArray)
-
-    console.log(myArray[visty])
-
-    myArray[visty].isCompleted = true
-
-    ban.removeChild(bana)
-    ban.removeChild(banb)
-    ban.removeChild(banc)
-    ban.removeChild(band)
-
-    console.log(myArray)
-    console.log(myArray[visty])
-
-  }
-
-  banj.addEventListener("click",ross,false)
-
+    var myStorage = sessionStorage.setItem("toDoArray", stringifiedArray)
 
 }
-//Function for Checkbox button in list items
+var submitButton = document.getElementById("submit")
 
-
-var house = document.getElementById("gru")
-
-
-//id: toDoItem[toDoItem.length - 1].id + 1
-
-house.addEventListener("click",function(e) {
-  e.preventDefault();
-  toss()
-},false)
+submitButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    createRow()
+}, false)
