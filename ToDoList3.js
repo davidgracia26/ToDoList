82a2281c17b26d6e2693eb6717f1005ca90b42b4
davidgracia26 <david.gracia26@gmail.com>
2017-01-14 16:32:47 -0600

    Added semi-colons

--- ToDoList3.js
+++ ToDoList3.js
@@ -13,26 +13,26 @@ window.onload = function() {
         for (var i = 0; i < parsedToDoArray.length; i++) {
 
             //Creates the list row on the page with entry
-            var row = document.createElement("tr")
-            var td1 = document.createElement("td")
-            var td2 = document.createElement("td")
-            var td3 = document.createElement("td")
-            var td4 = document.createElement("td")
-            var input = document.createElement("input")
-            input.setAttribute("type", "checkbox")
+            var row = document.createElement("tr");
+            var td1 = document.createElement("td");
+            var td2 = document.createElement("td");
+            var td3 = document.createElement("td");
+            var td4 = document.createElement("td");
+            var input = document.createElement("input");
+            input.setAttribute("type", "checkbox");
 
-            var tn1 = document.createTextNode(parsedToDoArray[i].id)
-            var tn2 = document.createTextNode(parsedToDoArray[i].listItem)
-            var tn3 = document.createTextNode(parsedToDoArray[i].dateCreated)
-            // var rowh = document.createTextNode(input)
+            var tn1 = document.createTextNode(parsedToDoArray[i].id);
+            var tn2 = document.createTextNode(parsedToDoArray[i].listItem);
+            var tn3 = document.createTextNode(parsedToDoArray[i].dateCreated);
 
-            row.appendChild(td1).appendChild(tn1)
-            row.appendChild(td2).appendChild(tn2)
-            row.appendChild(td3).appendChild(tn3)
-            row.appendChild(td4).appendChild(input)
+
+            row.appendChild(td1).appendChild(tn1);
+            row.appendChild(td2).appendChild(tn2);
+            row.appendChild(td3).appendChild(tn3);
+            row.appendChild(td4).appendChild(input);
 
             //Adds list row to page
-            document.getElementById("movingTable").appendChild(row)
+            document.getElementById("movingTable").appendChild(row);
         }
     }
 }
@@ -58,35 +58,34 @@ function createRow() {
     }
 
     //Creates the list row on the page with entry
-    var row = document.createElement("tr")
-    var td1 = document.createElement("td")
-    var td2 = document.createElement("td")
-    var td3 = document.createElement("td")
-    var td4 = document.createElement("td")
-    var input = document.createElement("input")
-    input.setAttribute("type", "checkbox")
+    var row = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
+    var td4 = document.createElement("td");
+    var input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
 
-    var tn1 = document.createTextNode(myArray[myArray.length - 1].id)
-    var tn2 = document.createTextNode(myArray[myArray.length - 1].listItem)
-    var tn3 = document.createTextNode(myArray[myArray.length - 1].dateCreated)
-    // var rowh = document.createTextNode(input)
+    var tn1 = document.createTextNode(myArray[myArray.length - 1].id);
+    var tn2 = document.createTextNode(myArray[myArray.length - 1].listItem);
+    var tn3 = document.createTextNode(myArray[myArray.length - 1].dateCreated);
 
-    row.appendChild(td1).appendChild(tn1)
-    row.appendChild(td2).appendChild(tn2)
-    row.appendChild(td3).appendChild(tn3)
-    row.appendChild(td4).appendChild(input)
+    row.appendChild(td1).appendChild(tn1);
+    row.appendChild(td2).appendChild(tn2);
+    row.appendChild(td3).appendChild(tn3);
+    row.appendChild(td4).appendChild(input);
 
     //Adds list row to page
-    document.getElementById("movingTable").appendChild(row)
+    document.getElementById("movingTable").appendChild(row);
 
     var stringifiedArray = JSON.stringify(myArray);
 
-    var myStorage = sessionStorage.setItem("toDoArray", stringifiedArray)
+    var myStorage = sessionStorage.setItem("toDoArray", stringifiedArray);
 
 }
-var submitButton = document.getElementById("submit")
+var submitButton = document.getElementById("submit");
 
 submitButton.addEventListener("click", function(e) {
     e.preventDefault();
-    createRow()
+    createRow();
 }, false)
